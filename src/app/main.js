c60eb742a59b87dd4809a49c1aff77f8dace1521
abhinaw <abhinaw27392@gmail.com>
2018-01-26 14:14:15 +0530

    first commit

---
+++ src/app/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home.jsx'
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <Home />
+         </div>
+      );
+   }
+}
+ReactDOM.render(<App />, document.getElementById('app'));
